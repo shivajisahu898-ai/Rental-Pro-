64e465d39cd5bd31f95107bb31bac4f43b371149
shivajisahu898-ai <shivajisahu898@gmail.com>
2026-09-12 10:50:23 +0530

    Update and rename app/driver/page .js to driver/page.js

--- app/driver/page .js
+++
@@ -1,9 +0,0 @@
-export default function Home(){
-  return (
-    <div style={{padding:20, fontFamily:'sans-serif'}}>
-      <h1>Rental Pro - Live</h1>
-      <p><a href="/driver">Driver Panel</a></p>
-      <p><a href="/track">Track Ride</a></p>
-    </div>
-  )
-}
